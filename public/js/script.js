830acbc0bd54d686737bddd23e9ec3556ec98df9
unknown <muflihunnaim275@gmail.com>
2021-02-21 04:49:20 +0800

    much better

--- public/js/script.js
+++ public/js/script.js
@@ -3,39 +3,70 @@ const pconsole = document.getElementById('console');
 const btnAddLabel = document.getElementById('addLabel');
 const btnAddMe = document.getElementById('addMe');
 const inputLabel = document.getElementById('inputLabel');
+const canvas = document.getElementById('canvas');
 
-let facesRegistered = {}; // {label1: desc1, label2: desc2, ...}
+const displaySize = { width: webcam.width, height: webcam.height };
+let showFaceLocation = false;
+let showFaceLandmarks = false;
+
+let facesRegistered = []; // [{label:* , desc:*}, {...}, ...]
 let myFaceDescriptors = []; // [desc1, des2, ...]
 
-async function main(){
+async function main() {
     console.log('loading the model..');
-    // await faceapi.loadSsdMobilenetv1Model('/model');
-    await faceapi.loadTinyFaceDetectorModel('/model');
-    await faceapi.loadFaceLandmarkTinyModel('/model');
+    await faceapi.loadSsdMobilenetv1Model('/model');
+    // await faceapi.loadTinyFaceDetectorModel('/model');
+    await faceapi.loadFaceLandmarkModel('/model');
     await faceapi.loadFaceRecognitionModel('/model');
     console.log('successfully loaded model');
 
-    console.log(faceapi.nets);
-
     console.log('start detecting faces..');
-    setInterval( async () => {
-        const useTinyModel = true;
+    // Start looping for classify
+    setInterval(async () => {
+        // detect faces
         const detections = await faceapi
-            .detectAllFaces(webcam, new faceapi.TinyFaceDetectorOptions())
-            .withFaceLandmarks(useTinyModel).withFaceDescriptors();
+            .detectAllFaces(webcam)
+            .withFaceLandmarks()
+            .withFaceDescriptors();
+
+        if (showFaceLocation){
+            // resize the detected boxes in case your displayed image has a different size then the original
+            const detectionsForSize = faceapi.resizeResults(detections, displaySize);
+            // draw them into a canvas
+            canvas.width = webcam.width;
+            canvas.height = webcam.height;
+            const showDetectionScore = false;
+            faceapi.draw.drawDetections(canvas, detectionsForSize, { withScore: showDetectionScore });
+        }
+
+        if (showFaceLandmarks){
+            // resize the detected boxes and landmarks in case your displayed image has a different size then the original
+            const detectionsWithLandmarksForSize = faceapi.resizeResults(detections, displaySize);
+            // draw them into a canvas
+            canvas.width = webcam.width;
+            canvas.height = webcam.height;
+            faceapi.draw.drawFaceLandmarks(canvas, detectionsWithLandmarksForSize, { drawLines: true });
+        }
+
         var result = null;
-        if (detections.length && myFaceDescriptors.length){
+        if (detections.length && (myFaceDescriptors.length || facesRegistered.length)) {
             // ada wajah terdeteksi
 
             // init my face labeled descriptor
-            const myLabel = 'This is Me!!'
-            const myFaceLabeled = new faceapi.LabeledFaceDescriptors(
-                myLabel,
-                myFaceDescriptors
-            );
+            var myFaceLabeled = null;
+            const myLabel = 'This is Me!!';
+            var faceMatcher = null;
+            if (myFaceDescriptors.length) {
+                myFaceLabeled = new faceapi.LabeledFaceDescriptors(
+                    myLabel,
+                    myFaceDescriptors
+                );
+                faceMatcher = new faceapi.FaceMatcher([...facesRegistered, myFaceLabeled]);
+            } else {
+                faceMatcher = new faceapi.FaceMatcher(facesRegistered);
+            }
             // console.log('My Face Labeled: ', myFaceLabeled);
 
-            const faceMatcher = new faceapi.FaceMatcher(myFaceLabeled);
             detections.forEach(desc => {
                 // console.log('Descriptor: \n', desc);
                 const bestMatch = faceMatcher.findBestMatch(desc.descriptor);
@@ -48,15 +79,15 @@ async function main(){
             result = 'No';
         }
         pconsole.innerText = `${result} face detected!`;
-    }, 100);
+    }, 10);
 }
 
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices.getDisplayMedia({
     video: {}, audio: false
 }).then(stream => {
     webcam.srcObject = stream;
-    webcam.width = 224;
-    webcam.height = 224;
+    webcam.clientWidth = 224;
+    webcam.clientHeight = 224;
 });
 
 webcam.addEventListener('play', () => {
@@ -70,10 +101,10 @@ btnAddMe.addEventListener('click', () => {
 
 btnAddLabel.addEventListener('click', async () => {
     const newLabel = inputLabel.value;
-    const useTinyModel = true;
     const newDescriptor = await faceapi
-        .detectSingleFace(webcam, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks(useTinyModel).withFaceDescriptor();
+        .detectSingleFace(webcam)
+        .withFaceLandmarks()
+        .withFaceDescriptor();
 
     console.log(`${newLabel} Descriptor: `, newDescriptor);
 
@@ -81,19 +112,24 @@ btnAddLabel.addEventListener('click', async () => {
 
     /**
      * TODO: simpan newLabel and newDescriptor ke db
-     * ...
+     * ...fgfhffgffgfgfg
      */
 });
 
 function registerNewFace(label, descriptors) {
-    facesRegistered[label] = descriptors;
+    const newFace = new faceapi.LabeledFaceDescriptors(
+        label,
+        descriptors
+    );
+    facesRegistered.push(newFace);
     console.log('Faces Registered: ', facesRegistered);
 }
 
 async function registerMyFace() {
     const myNewDescriptor = await faceapi
-        .detectSingleFace(webcam, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks(true).withFaceDescriptor();
+        .detectSingleFace(webcam)
+        .withFaceLandmarks()
+        .withFaceDescriptor();
 
     myFaceDescriptors.push(myNewDescriptor.descriptor);
     console.log(`My Face Descriptor: `, myFaceDescriptors);
